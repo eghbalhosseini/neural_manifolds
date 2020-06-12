9fa60b162a649838570b05807def25b538a1e129
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-12 10:17:08 -0400

    bugfix

--- om_neural_manifold_create_parition_dataset.sh
+++ om_neural_manifold_create_parition_dataset.sh
@@ -5,8 +5,8 @@
 #SBATCH --job-name=PARTITION
 #SBATCH -t 18:00:00
 #SBATCH -N 1
-#SBATCH -n 12
-#SBATCH --mem-per-cpu 10000
+#SBATCH -n 8
+#SBATCH --mem-per-cpu 32000
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=ehoseini@mit.edu
