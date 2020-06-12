76c5a700895d34a9291e6ec4b7e4cd056581cca2
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-12 10:01:03 -0400

    added paralellism to the code

--- om_neural_manifold_create_parition_dataset.sh
+++ om_neural_manifold_create_parition_dataset.sh
@@ -3,7 +3,8 @@
 #  om_neural_manifold_create_partiion_dataset.sh
 #SBATCH --output=partition_data.out
 #SBATCH --job-name=PARTITION
-#SBATCH -t 12:00:00
+#SBATCH -t 18:00:00
+#SBATCH -N 1
 #SBATCH -n 12
 #SBATCH --mem-per-cpu 10000
 #SBATCH --exclude node017,node018
@@ -12,4 +13,4 @@
 
 
 module add mit/matlab/2020a
-matlab -nodisplay -signelCompThread -r "addpath('/home/ehoseini/MyCodes/neural_manifolds/');addpath(genpath('/home/ehoseini/MyCodes/neural_manifolds/'));neural_manifold_create_partiion_dataset;quit;"
+matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);addpath('/home/ehoseini/MyCodes/neural_manifolds/');addpath(genpath('/home/ehoseini/MyCodes/neural_manifolds/'));neural_manifold_create_partition_dataset;quit;"
