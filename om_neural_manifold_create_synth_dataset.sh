c0af588a770ac38606bc5aed2be74cf2c25934a3
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-20 15:56:22 -0400

    test

--- om_neural_manifold_create_synth_dataset.sh
+++ om_neural_manifold_create_synth_dataset.sh
@@ -17,7 +17,7 @@
 i=0
 
 for struct in 'partition' 'tree' ; do
-  struct_list[$i]="$struct"
+  struct_list[$i]=$struct
   i=$i+1
 done
 
@@ -30,6 +30,6 @@ addpath(genpath('/home/ehoseini/MyCodes/neural_manifolds/'));\
 save_path='/om/user/ehoseini/MyData/neural_manifolds/';\
 n_class=50;exm_per_class=1000;n_feat=3*32*32;\
 beta=0.01;sigma=1.5;\
-fprintf('creating structure %s\n',"${struct_list[$SLURM_ARRAY_TASK_ID]}");\
+fprintf('creating structure %s\n',char("${struct_list[$SLURM_ARRAY_TASK_ID]}"));\
 neural_manifold_create_synth_data_cholesky_method('structure',char(${struct_list[$SLURM_ARRAY_TASK_ID]}),'n_class',n_class,'exm_per_class',exm_per_class,'n_feat',n_feat,'save_path',save_path,'beta',beta,'sigma',sigma);\
 quit;"
