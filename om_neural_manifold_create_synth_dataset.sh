48afb50bb9849bf0021f8954fe2f490034c6462e
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-20 17:16:11 -0400

    bugfix

--- om_neural_manifold_create_synth_dataset.sh
+++ om_neural_manifold_create_synth_dataset.sh
@@ -5,7 +5,7 @@
 #SBATCH --job-name=SYNTH
 #SBATCH -t 1:00:00
 #SBATCH -N 1
-#SBATCH --array=0-1
+#SBATCH --array=1-2
 #SBATCH -n 8
 #SBATCH --mem-per-cpu 32000
 #SBATCH --exclude node017,node018
@@ -22,6 +22,6 @@ save_path='/om/user/ehoseini/MyData/neural_manifolds/';\
 structures={'partition','tree'};\
 n_class=50;exm_per_class=5000;n_feat=3*32*32;\
 beta=0.01;sigma=1.5;\
-fprintf('creating structure %s\n',structures{$SLURM_ARRAY_TASK_ID+1});\
-neural_manifold_create_synth_data_cholesky_method('structure',structures{$SLURM_ARRAY_TASK_ID+1},'n_class',n_class,'exm_per_class',exm_per_class,'n_feat',n_feat,'save_path',save_path,'beta',beta,'sigma',sigma);\
+fprintf('creating structure %s\n',structures{$SLURM_ARRAY_TASK_ID});\
+neural_manifold_create_synth_data_cholesky_method('structure',structures{$SLURM_ARRAY_TASK_ID},'n_class',n_class,'exm_per_class',exm_per_class,'n_feat',n_feat,'save_path',save_path,'beta',beta,'sigma',sigma);\
 quit;"
