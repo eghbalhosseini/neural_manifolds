95f55a4c6f33036d677af3cd91737d88bf2588e0
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-21 17:35:39 -0400

    training paradigm for VGG and synthetic data

--- neural_manifold_utils.py
+++ neural_manifold_utils.py
@@ -2,8 +2,6 @@ import torch
 import numpy as np
 from torch.utils.data import Dataset
 import mat73
-import scipy
-import scipy.io as sio
 from scipy.io import loadmat
 
 class CFAR100_fake_dataset(Dataset):
@@ -14,9 +12,10 @@ class CFAR100_fake_dataset(Dataset):
     def __len__(self):
         return self.n_samples
     def __getitem__(self, idx):
+        #item=np.expand_dims(self.dat[idx],axis=0)
         item=self.dat[idx]
         targ=np.squeeze(self.target[idx])
-        return (torch.tensor(item,dtype=torch.float), targ)
+        return torch.tensor(item,dtype=torch.float), torch.tensor(targ,dtype=torch.long)
     def load_data(self):
         try:
             annot=loadmat(self.data_dir)
@@ -27,8 +26,57 @@ class CFAR100_fake_dataset(Dataset):
             vals=data_dict['ops_out']
         dat=vals['data']
         self.vals=vals
-        self.adj=vals['Adjacency']
+        self.beta=float(vals['beta'])
+        self.sigma = float(vals['sigma'])
+        self.data_latent=vals['data_latent']
+        self.exm_per_class=int(vals['exm_per_class'])
+        self.n_class=int(vals['n_class'])
+        self.n_feat=int(vals['n_feat'])
+        self.n_latent=int(vals['n_latent'])
+        self.is_norm=bool(vals['norm'])
+        self.structure = str(vals['structure'])
         dat_new=dat[:,range(3*32*32)]
         dat_new=np.reshape(dat_new,(-1,3,32,32))
         target=np.double(np.transpose(vals['class_id'])-1.0)
+        # add extra component defining the graph and dataset.
         return dat_new, target
+
+def train(model, device, train_loader, optimizer):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        output = torch.nn.functional.log_softmax(output, dim=1)
+        loss = torch.nn.functional.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if (batch_idx % 1000 == 0) & (batch_idx!=0):
+            pred = output.argmax(dim=1, keepdim=True)
+            correct = pred.eq(target.view_as(pred)).sum().item()
+            print('Train Epoch: [{}/{} ({:.0f}%)]\Loss: {:.6f}, Train Accuracy: ({:.0f}%)'.format(
+                 batch_idx * len(data), len(train_loader.dataset),
+                100. * batch_idx / len(train_loader), loss.item(),
+                 100. * correct / len(target)))
+
+
+def test(model, device, test_loader, epoch):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            output = torch.nn.functional.log_softmax(output, dim=1)
+            test_loss += torch.nn.functional.nll_loss(output, target, reduction='sum').item()
+            pred = output.argmax(dim=1, keepdim=True)
+            correct += pred.eq(target.view_as(pred)).sum().item()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set epoch {}: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        epoch, test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    test_acu = 100. * correct / len(test_loader.dataset)
+    return test_acu
--- train_network_on_synthetic_data.py
+++ train_network_on_synthetic_data.py
@@ -1,35 +1,79 @@
 from __future__ import print_function
-import numpy as np
 import argparse
 import torch
-import torchvision
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms, models
-from torch.autograd import Variable
+from torchvision import models
 from torch.utils.tensorboard import SummaryWriter
-from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import Dataset
-import scipy
-import scipy.io as sio
-from scipy.io import loadmat
-import os, sys
+from neural_manifold_utils import CFAR100_fake_dataset, train, test
+import os
 import socket
+from datetime import datetime
 import getpass
-import mat73
-print('__cuda available ',torch.cuda.is_available())
-print('__Python VERSION:', sys.version)
-print('__pyTorch VERSION:', torch.__version__)
-print('__CUDNN VERSION:', torch.backends.cudnn.version())
-print('__Number CUDA Devices:', torch.cuda.device_count())
-print('__Device name:', torch.cuda.get_device_name(0))
+
 
 user=getpass.getuser()
 print(user)
 if user=='eghbalhosseini':
     save_dir='/Users/eghbalhosseini/MyData/neural_manifolds/network_training_on_synthetic/'
-    data_dir='/Users/eghbalhosseini/MyData/neural_manifolds/synthetic_dataset/'
+    data_dir='/Users/eghbalhosseini/MyData/neural_manifolds/synthetic_datasets/'
 elif user=='ehoseini':
     save_dir='/om/user/ehoseini/MyData/neural_manifolds/network_training_on_synthetic/'
     data_dir='/om/user/ehoseini/MyData/neural_manifolds/synthetic_datasets/'
+
+parser = argparse.ArgumentParser(description='neural manifold train network')
+parser.add_argument('datafile', type=str, default="synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat",help='')
+args=parser.parse_args()
+
+if __name__=='__main__':
+    # load dataset
+    train_batch_size = 32
+    test_batch_size = 1024
+    epochs = 25
+    train_dataset = CFAR100_fake_dataset(data_dir=os.path.join(data_dir, args.datafile))
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(train_dataset, batch_size=test_batch_size, shuffle=True)
+    # specs
+    train_spec = {'train_batch_size': train_batch_size,
+                  'test_batch_size': test_batch_size,
+                  'num_epochs': epochs,
+                  'structure': train_dataset.structure,
+                  'n_class': train_dataset.n_class,
+                  'exm_per_class': train_dataset.exm_per_class,
+                  'beta': train_dataset.beta,
+                  'sigma': train_dataset.sigma,
+                  'norm': train_dataset.is_norm
+                  }
+
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    # build model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = models.vgg16(num_classes=train_dataset.n_class)
+    model = model.to(device)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+    # create train log for tensorflow
+    current_time = datetime.now().strftime('%b%d_%H-%M-%S')
+    log_dir = os.path.join(save_dir, 'runs', current_time + '_' + socket.gethostname())
+    #writer = SummaryWriter(log_dir=log_dir)
+    #writer.add_hparams(hparam_dict=train_spec, metric_dict={})
+    # do the training :
+    for epoch in range(1, epochs + 1):
+        train(model, device, train_loader, optimizer)
+        test_accuracy = test(model, device, test_loader, epoch)
+        if test_accuracy > 70:
+            train_success=True
+            break
+        else:
+            train_success=False
+    #writer.flush()
+    #writer.close()
+    # save the model in case it was successful
+    model_save_path = save_dir + 'VGG16_synthdata_' + train_dataset.structure + '_nclass_' + str(
+        int(train_dataset.n_class)) + '_n_exm_' + str(int(train_dataset.exm_per_class))
+    if train_success:
+        torch.save(model.state_dict(), model_save_path)
+    else:
+        print('unsuccessful training')
+    # end of file.
+
