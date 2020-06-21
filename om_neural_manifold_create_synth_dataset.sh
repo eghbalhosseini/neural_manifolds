c9c237b76f9d1bed6df8b26a2925d61b47d39ab1
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-21 15:36:07 -0400

    bugfix

--- om_neural_manifold_create_synth_dataset.sh
+++ om_neural_manifold_create_synth_dataset.sh
@@ -32,9 +32,9 @@ matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);addpath('/home/ehoseini/M
 addpath(genpath('/home/ehoseini/MyCodes/neural_manifolds/'));\
 save_path='/om/user/ehoseini/MyData/neural_manifolds/synthetic_datasets/';\
 structures={'partition','tree'};\
-fprintf('creating structure %s\n',structures{$SLURM_ARRAY_TASK_ID});\
-struct=structures{$struct_list[$SLURM_ARRAY_TASK_ID]};\
-n_class=$n_class_list[$SLURM_ARRAY_TASK_ID];\
+fprintf('creating structure %s\n',structures{${struct_list[$SLURM_ARRAY_TASK_ID]}});\
+struct=structures{${struct_list[$SLURM_ARRAY_TASK_ID]}};\
+n_class=${n_class_list[$SLURM_ARRAY_TASK_ID]};\
 exm_per_class=1000;n_feat=3*32*32;\
 beta=0.01;sigma=1.5;\
 neural_manifold_create_synth_data_cholesky_method('structure',struct,'n_class',n_class,'exm_per_class',exm_per_class,'n_feat',n_feat,'save_path',save_path,'beta',beta,'sigma',sigma);\
