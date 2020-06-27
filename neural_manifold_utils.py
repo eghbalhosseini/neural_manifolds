2097efb24dddd54e87e60b55869a8321a0501f88
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-27 09:50:04 -0400

    bugfix

--- neural_manifold_utils.py
+++ neural_manifold_utils.py
@@ -4,6 +4,9 @@ from torch.utils.data import Dataset
 import mat73
 from scipy.io import loadmat
 import pickle
+from collections import defaultdict
+import torch
+import numpy as np
 
 class CFAR100_fake_dataset_mftma(Dataset):
     def __init__(self, data_dir=None):
@@ -125,6 +128,59 @@ class sub_data(Dataset):
         data_tensor = torch.from_numpy(single_data)
         data_tensor = data_tensor.type(torch.FloatTensor)
         return data_tensor, target
+
+
+
+
+def create_manifold_data(dataset, sampled_classes, examples_per_class, max_class=None, seed=0):
+    '''
+    Samples manifold data for use in later analysis
+
+    Args:
+        dataset: PyTorch style dataset, or iterable that contains (input, label) pairs
+        sampled_classes: Number of classes to sample from (must be less than or equal to
+            the number of classes in dataset)
+        examples_per_class: Number of examples per class to draw (there should be at least
+            this many examples per class in the dataset)
+        max_class (optional): Maximum class to sample from. Defaults to sampled_classes if unspecified
+        seed (optional): Random seed used for drawing samples
+
+    Returns:
+        data: Iterable containing manifold input data
+    '''
+    if max_class is None:
+        max_class = sampled_classes
+    assert sampled_classes <= max_class, 'Not enough classes in the dataset'
+    assert examples_per_class * max_class <= len(dataset), 'Not enough examples per class in dataset'
+
+    # Set the seed
+    np.random.seed(0)
+    # Storage for samples
+    sampled_data = defaultdict(list)
+    # Sample the labels
+    sampled_labels = np.random.choice(list(range(max_class)), size=sampled_classes, replace=False)
+    # Shuffle the order to iterate through the dataset
+    idx = [i for i in range(len(dataset))]
+    np.random.shuffle(idx)
+    # Iterate through the dataset until enough samples are drawn
+    for i in idx:
+        sample, label = dataset[i]
+        if int(label) in sampled_labels and len(sampled_data[label]) < examples_per_class:
+            sampled_data[label].append(sample)
+        # Check if enough samples have been drawn
+        complete = True
+        for s in sampled_labels:
+            if len(sampled_data[s]) < examples_per_class:
+                complete = False
+        if complete:
+            break
+    # Check that enough samples have been found
+    assert complete, 'Could not find enough examples for the sampled classes'
+    # Combine the samples into batches
+    data = []
+    for s, d in sampled_data.items():
+        data.append(torch.stack(d))
+    return data
 
 def train(epoch,model, device, train_loader,test_loader, optimizer,train_spec):
     model.train()
