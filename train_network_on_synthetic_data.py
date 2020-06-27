b653259366d0a331f52475ec4dd83c55ea0aa556
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-06-27 12:28:03 -0400

    new routine for training with embedded activation extractions

--- train_network_on_synthetic_data.py
+++ train_network_on_synthetic_data.py
@@ -5,18 +5,20 @@ import copy
 from torchvision import models
 from torch.utils.tensorboard import SummaryWriter
 from torch.utils.data import Dataset
-from mftma.utils.make_manifold_data import make_manifold_data
-from mftma.utils.activation_extractor import extractor
-from neural_manifold_utils import CFAR100_fake_dataset, CFAR100_fake_dataset_mftma, train, test, save_dict
+from neural_manifold_utils import train, test, save_dict, sub_data, create_manifold_data
+from torch.utils.data.sampler import SubsetRandomSampler
 import os, sys
 import socket
 from datetime import datetime
 import getpass
+import numpy as np
+
+from mftma.utils.activation_extractor import extractor
 print('__cuda available ',torch.cuda.is_available())
 print('__Python VERSION:', sys.version)
 print('__CUDNN VERSION:', torch.backends.cudnn.version())
 print('__Number CUDA Devices:', torch.cuda.device_count())
-print('__Device name:', torch.cuda.get_device_name(0))
+
 
 user=getpass.getuser()
 print(user)
@@ -27,6 +29,8 @@ elif user=='ehoseini':
     save_dir='/om/user/ehoseini/MyData/neural_manifolds/network_training_on_synthetic/'
     data_dir='/om/user/ehoseini/MyData/neural_manifolds/synthetic_datasets/'
 
+if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
 parser = argparse.ArgumentParser(description='neural manifold train network')
 parser.add_argument('datafile', type=str, default="synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat",help='')
 args=parser.parse_args()
@@ -34,85 +38,99 @@ args=parser.parse_args()
 if __name__=='__main__':
     # load dataset
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    train_batch_size = 32
-    test_batch_size = 1024
-    epochs = 100
-    exm_per_class=100
-    train_dataset = CFAR100_fake_dataset(data_dir=os.path.join(data_dir, args.datafile))
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=train_batch_size, shuffle=True)
-    test_loader = torch.utils.data.DataLoader(train_dataset, batch_size=test_batch_size, shuffle=True)
-
-    train_dataset_mtmfa = CFAR100_fake_dataset_mftma(data_dir=os.path.join(data_dir, args.datafile))
-    num_hierarchy=len(train_dataset_mtmfa.vals.hierarchical_class_ids)
-    hier_classes = [x - 1 for x in train_dataset_mtmfa.vals.hierarchical_class_ids]
+    batch_size_train = 64
+    batch_size_test = 64
+    resize = True
+    epochs = 2
+    exm_per_class = 100
+    save_epochs = False
+    momentum = 0.5
+    lr = 0.01
+    gamma = 0.7
+    log_interval = 75
+    test_split = .2
+    shuffle_dataset = True
+    random_seed = 1
+    #
+    dataset = sub_data(data_path=os.path.join(data_dir, args.datafile))
+    dataset_size = len(dataset)
+    indices = list(range(dataset_size))
+    split = int(np.floor(test_split * dataset_size))
+    if shuffle_dataset:
+        np.random.seed(random_seed)
+        np.random.shuffle(indices)
+    train_indices, test_indices = indices[split:], indices[:split]
+    # create train test splits
+    train_sampler = SubsetRandomSampler(train_indices)
+    test_sampler = SubsetRandomSampler(test_indices)
+    train_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size_train, sampler=train_sampler)
+    test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size_test, sampler=test_sampler)
+    # create manifold analysis dataset:
+    num_hierarchy = len(dataset.hierarchical_target)
+    hier_classes = [x.astype(int) for x in dataset.hierarchical_target]
     hier_n_class = [int(max(x) + 1) for x in hier_classes]
-    hier_dat_mtmfa=[]
-
+    hier_dataset = []
     for idx, x in enumerate(hier_classes):
         dat_mfmta = []
-        dat_mtmfa = copy.deepcopy(train_dataset_mtmfa)
-        dat_mtmfa.target = hier_classes[idx]
-        hier_dat_mtmfa.append(copy.deepcopy(dat_mtmfa));
-    hier_sample_mtmfa= [make_manifold_data(x, hier_n_class[idx], exm_per_class, seed=0) for idx, x in enumerate(hier_dat_mtmfa)]
-    # specs
-    train_spec = {'train_batch_size': train_batch_size,
-                  'test_batch_size': test_batch_size,
+        dat_mtmfa = copy.deepcopy(dataset)
+        dat_mtmfa.targets = hier_classes[idx]
+        hier_dataset.append(copy.deepcopy(dat_mtmfa))
+
+    hier_sample_mtmfa = [create_manifold_data(x, hier_n_class[idx], exm_per_class, seed=0) for idx, x in enumerate(hier_dataset)]
+    hier_sample_mtmfa = [[d.to(device) for d in data] for data in hier_sample_mtmfa]
+
+    # define train specs and model
+    train_spec = {'train_batch_size': batch_size_train,
+                  'test_batch_size': batch_size_test,
                   'num_epochs': epochs,
-                  'structure': train_dataset.structure,
-                  'n_class': train_dataset.n_class,
-                  'exm_per_class': train_dataset.exm_per_class,
-                  'beta': train_dataset.beta,
-                  'sigma': train_dataset.sigma,
-                  'norm': train_dataset.is_norm
+                  'structure': dataset.structure,
+                  'n_class': dataset.n_class,
+                  'exm_per_class': dataset.exm_per_class,
+                  'beta': dataset.beta,
+                  'sigma': dataset.sigma,
+                  'norm': dataset.is_norm,
+                  'log_interval': log_interval,
+                  'is_cuda': torch.cuda.is_available()
                   }
 
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-    # build model
-    model = models.vgg16(num_classes=train_dataset.n_class)
+    model = models.vgg16(num_classes=dataset.n_class)
     model = model.to(device)
     optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
-    # create train log for tensorflow
     current_time = datetime.now().strftime('%b%d_%H-%M-%S')
     log_dir = os.path.join(save_dir, 'runs', current_time + '_' + socket.gethostname())
-    #writer = SummaryWriter(log_dir=log_dir)
-    #writer.add_hparams(hparam_dict=train_spec, metric_dict={})
-    # do the training :
-    train_test_accu=[]
+    model_dir = save_dir + 'train_VGG16_synthdata_' + dataset.structure + '_nclass_' + str(
+        int(dataset.n_class)) + '_n_exm_' + str(int(dataset.exm_per_class))
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    # training :
+
     for epoch in range(1, epochs + 1):
-        train_accuracy = train(model, device, train_loader, optimizer)
+        data_ = {'activations_cell': [],
+                 'train_spec': train_spec,
+                 'train_accuracy': [],
+                 'test_accuracy': [],
+                 'train_success': False,
+                 'epoch': epoch}
+        train_test_data = []
+        epoch_dat = train(epoch, model, device, train_loader, test_loader, optimizer, train_spec)
         test_accuracy = test(model, device, test_loader, epoch)
+        if test_accuracy > 70:
+            train_success = True
+        else:
+            train_success = False
         # extract activation
         model = model.eval()
-        activations_cell = [extractor(model,x,layer_types=['Conv2d', 'Linear']) for x in hier_sample_mtmfa]
-        if test_accuracy > 70:
-            train_success=True
-            train_test_accu.append([train_accuracy, test_accuracy, train_success])
+        activations_cell = [extractor(model, x, layer_types=['Conv2d', 'Linear']) for x in hier_sample_mtmfa]
+        data_['train_accuracy']=epoch_dat['train_acc']
+        data_['test_accuracy']=test_accuracy
+        data_['activations_cell'] = activations_cell
+        data_['train_success'] = train_success
+        epoch_save_path = os.path.join(model_dir, 'train_epoch_' + str(epoch))
+        save_dict(data_, epoch_save_path)
+        if train_success:
+            print('successful training')
             break
-        else:
-            train_success=False
-            train_test_accu.append([train_accuracy, test_accuracy,train_success])
 
-    data_ = {'activations_cells': activations_cell,
-             'exm_per_class': exm_per_class,
-             'num_hierarchy': num_hierarchy,
-             'train_test_accuracy': train_test_accu,
-             'structure':train_dataset.structure,
-             'train_success':train_success
-             }
 
-    result_save_path = save_dir + 'mftma_data_VGG16_synthdata_' + data_['structure'] + '_n_hier_' + str(data_['num_hierarchy']) \
-                       + '_exm_per_class_' + str(data_['exm_per_class']) +'train_success' +str(data_['train_success'])
-    save_dict(data_, result_save_path)
-    # save the model in case it was successful
-    model_save_path = save_dir + save_dir + 'VGG16_synthdata_' + data_['structure'] + '_n_hier_' + str(data_['num_hierarchy']) \
-                       + '_exm_per_class_' + str(data_['exm_per_class']) +'train_success' +str(data_['train_success'])
-    if train_success:
-        torch.save(model.state_dict(), model_save_path)
-    else:
-        print('unsuccessful training')
-
-    # to do : add a function for extracting activations,
 
 
