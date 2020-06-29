ed18de60dd4ea33e57b1e79410b9835f4d6397df
Greta Tuckute <greta-t@hotmail.com>
2020-06-29 17:37:16 -0400

    Update om_mfta_epoch_data_gt.sh

--- om_mfta_epoch_data_gt.sh
+++ om_mfta_epoch_data_gt.sh
@@ -2,13 +2,14 @@
 #SBATCH --job-name=mftma_epoch
 #SBATCH --output=mftma_epoch_%j.out
 #SBATCH --error=mftma_epoch_%j.err
-#SBATCH --mem=60G
+#SBATCH --mem=20G
 #SBATCH --nodes=1
 #SBATCH -t 04:00:00
 #SBATCH -c 1
+#SBATCH -w node[001-016]
 
-echo "Running training  ${1}"
-echo "Running epoch ${2}"
+# echo "Running training  ${1}"
+# echo "Running epoch ${2}"
 
 timestamp() {
   date +"%T"
@@ -21,12 +22,3 @@ cd /om/user/`whoami`/neural_manifolds/
 python run_mftma_on_epoch_data_noargs.py > "$filename"
 
 #python run_mftma_on_epoch_data.py "${1}" "${2}" > "$filename"
-
-#module add openmind/singularity
-#export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
-#RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
-#export RESULTCACHING_HOME
-#XDG_CACHE_HOME=/om/user/`whoami`/st
-#export XDG_CACHE_HOME
-
-#singularity exec --nv -B /om:/om /om/user/`whoami`/simg_images/neural_manifolds.simg python ~/MyCodes/neural_manifolds/run_mftma_on_epoch_data.py "${train_dir_list[$SLURM_ARRAY_TASK_ID]}" "${epoch_list[$SLURM_ARRAY_TASK_ID]}"
