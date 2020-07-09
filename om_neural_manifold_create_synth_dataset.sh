5b59e5726c9bfa2431acfba41f5e92c0ed44e450
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-07-09 13:46:33 -0400

    modified beta, sigma and num classes

--- om_neural_manifold_create_synth_dataset.sh
+++ om_neural_manifold_create_synth_dataset.sh
@@ -15,12 +15,18 @@
 #SBATCH --error=SYNTH_result_%j.err
 
 i=0
-for struct_id in 1 2 ; do
-for n_class in 50 100 ; do
-  struct_list[$i]=$struct_id
-  n_class_list[$i]=$n_class
-  i=$i+1
-done
+for beta_id in 1 2 ; do
+  for sigma_id in 1 2 ; do
+    for struct_id in 1 2 ; do
+      for n_class in 64 96 ; do
+        struct_list[$i]=$struct_id
+        beta_list[$i]=$beta_id
+        sigma_list[$i]=$sigma_id
+        n_class_list[$i]=$n_class
+        i=$i+1
+      done
+    done
+  done
 done
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
@@ -32,10 +38,12 @@ matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);addpath('/home/ehoseini/M
 addpath(genpath('/home/ehoseini/MyCodes/neural_manifolds/'));\
 save_path='/om/user/ehoseini/MyData/neural_manifolds/synthetic_datasets/';\
 structures={'partition','tree'};\
+betas=[1e-10,0.0222];\
+simgas=[0.8333,2.5];\
 fprintf('creating structure %s\n',structures{${struct_list[$SLURM_ARRAY_TASK_ID]}});\
 struct=structures{${struct_list[$SLURM_ARRAY_TASK_ID]}};\
 n_class=${n_class_list[$SLURM_ARRAY_TASK_ID]};\
 exm_per_class=1000;n_feat=3*32*32;\
-beta=0.01;sigma=1.5;\
+beta=betas(${beta_list[$SLURM_ARRAY_TASK_ID]});sigma=sigmas(${sigma_list[$SLURM_ARRAY_TASK_ID]});\
 neural_manifold_create_synth_data_cholesky_method('structure',struct,'n_class',n_class,'exm_per_class',exm_per_class,'n_feat',n_feat,'save_path',save_path,'beta',beta,'sigma',sigma);\
 quit;"
