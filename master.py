88b37e332e1d9b82ca2c71d3588c82775bd209b7
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-07-23 14:33:02 -0400

    fixed lazyload (removed it)

--- master.py
+++ master.py
@@ -5,9 +5,11 @@ Created on Wed Jul 22 11:08:09 2020
 @author: greta
 """
 import neural_manifold_utils
-from neural_manifold_utils import sub_data, NN_open, CNN_open, LazyLoad
+from neural_manifold_utils import sub_data, NN_open, CNN_open
 from torchvision import models
 import itertools
+import copy
+from importlib import import_module
 
 import os
 import getpass
@@ -24,18 +26,21 @@ elif user == 'gretatu':
     save_dir = '/om/user/gretatu/neural_manifolds/network_training_on_synthetic/'
     data_dir = '/om/user/ehoseini/MyData/neural_manifolds/synthetic_datasets/'
 
-
+def load_train(train_name):
+    return train_pool[train_name]()
 
 class params:
     def __init__(self,datafile="synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat",
                  model='CNN_open',
-                 train_type='train'):
+                 train_type='train',identifier=None):
         ##### DATA ####
         self.datafile=datafile
+        self.identifier=identifier
+        print('running')
         self.dataset= sub_data(data_path=os.path.join(data_dir, self.datafile))
         #
-        datafile = "synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat"
-        dataset = sub_data(data_path=os.path.join(data_dir, datafile))
+        #datafile = "synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat"
+        #dataset = sub_data(data_path=os.path.join(data_dir, datafile))
         exm_per_class = 100 # examples per class
         resize = True # reshape data into a 2D array # TODO make adaptable
         #### MODEL ####
@@ -45,9 +50,9 @@ class params:
         #### TRAINING ####
         self.train_type=train_type
 
-    model = CNN_open  # models.vgg16(num_classes=dataset.n_class) # or CNN etc
-    datafile = "synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat"
-    train_type = 'train_test'
+    #model = CNN_open  # models.vgg16(num_classes=dataset.n_class) # or CNN etc
+    #datafile = "synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat"
+    #train_type = 'train_test'
     batch_size_train = 64
     batch_size_test = 64
     epochs = 50
@@ -60,23 +65,33 @@ class params:
     random_seed = 1
     save_epochs = False # save individual mat files for each chosen epoch # GET RID OF?
 
+
+data_config={
+'partition/nclass=100/beta0.1/sigma1.5':'synth_partition_nobj_100000_nclass_100_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat',
+'partition/nclass=50/beta0.01/sigma1.5':'synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat',
+#'partition/nclass=64/beta0.00/sigma0.83':'synth_partition_nobj_64000_nclass_64_nfeat_3072_beta_0.00_sigma_0.83_norm_1.mat',
+#'partition/nclass=64/beta0.02/sigma0.83':'synth_partition_nobj_64000_nclass_64_nfeat_3072_beta_0.02_sigma_0.83_norm_1.mat',
+#'partition/nclass=64/beta0.02/sigma2.5':'synth_partition_nobj_64000_nclass_64_nfeat_3072_beta_0.02_sigma_2.50_norm_1.mat'
+}
 train_configuration=[]
-for dataset , model, train_type in itertools.product(
-        ['synth_partition_nobj_100000_nclass_100_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat',
-         'synth_partition_nobj_50000_nclass_50_nfeat_3072_beta_0.01_sigma_1.50_norm_1.mat',
-         'synth_partition_nobj_64000_nclass_64_nfeat_3072_beta_0.00_sigma_0.83_norm_1.mat',
-         'synth_partition_nobj_64000_nclass_64_nfeat_3072_beta_0.02_sigma_0.83_norm_1.mat',
-         'synth_partition_nobj_64000_nclass_64_nfeat_3072_beta_0.02_sigma_2.50_norm_1.mat',
-         'synth_partition_nobj_96000_nclass_96_nfeat_3072_beta_0.00_sigma_0.83_norm_1.mat'
-         'synth_partition_nobj_96000_nclass_96_nfeat_3072_beta_0.00_sigma_2.50_norm_1.mat',
-         'synth_partition_nobj_96000_nclass_96_nfeat_3072_beta_0.02_sigma_0.83_norm_1.mat',
-         'synth_partition_nobj_96000_nclass_96_nfeat_3072_beta_0.02_sigma_2.50_norm_1.mat']
-        ,['CNN_open','VGG16'],['train','train_test']):
-    train_configuration.append(dict(dataset=dataset,model=model,train_type=train_type))
+for dataset , model, train_type in itertools.product(list(data_config.keys()),['CNN_open'],['train']):
+    identifier=f"[{model}]-[{dataset}]-[{train_type}]"
+    train_configuration.append(dict(identifier=identifier,dataset=data_config[dataset],model=model,train_type=train_type))
 
-
+train_pool={}
 # create the pool
-#for config in train_configuration:
+for config in train_configuration:
+    configuration=copy.deepcopy(config)
+    identifier=configuration['identifier']
+    def train_instantionation(identfier=identifier,configure=frozenset(configuration.items())):
+        configure = dict(configure)
+        model=configure['model']
+        datafile=configure['dataset']
+        train_type=configure['train_type']
+        train_param=params(model=model,datafile=datafile,train_type=train_type)
+        return train_param
+
+    train_pool[identifier]=train_instantionation
 
 
 
