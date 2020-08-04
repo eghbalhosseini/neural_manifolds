232db59a1e63fe45524addb7378dfa7676ef9c98
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-08-04 16:44:19 -0400

    renaming folder names for utils

--- bash/extraction_pipeline.sh
+++ bash/extraction_pipeline.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-ROOT_DIR=/om/user/gretatu/neural_manifolds/network_training_on_synthetic
-PKL_FILE="master_NN-partition_nclass=50_nobj=50000_beta=0.01_sigma=1.5_nfeat=3072-train_test-test_performance.pkl"
+ROOT_DIR=/om/group/evlab/Greta_Eghbal_manifolds/extracted
 PTH_FILE="master_NN-partition_nclass=50_nobj=50000_beta=0.01_sigma=1.5_nfeat=3072-train_test-test_performance.txt"
+MODEL_ID='[NN]-[partition/nclass=50/nobj=50000/beta=0.01/sigma=1.5/nfeat=3072]-[train_test]-[test_performance]'
+ANALYZE_ID='[mftma]-[exm_per_class=20]-[proj=False]-[rand=True]-[kappa=0]-[n_t=300]-[n_rep=1]'
 
 FULL_FILE="${ROOT_DIR}/${PTH_FILE}"
 echo $FULL_FILE
@@ -16,7 +17,7 @@ while read line; do
 	if [ "$(expr ${LINE_COUNT} % 100)" = "0" ]
 	then
 		echo "New Array For Parameters from ${START_INDEX} to ${LINE_COUNT}"
-		sbatch --array=1-100 --mem 8G -p normal extraction_script.sh ${ARRAY_INDEX} ${FULL_FILE} ${PKL_FILE} ${ROOT_DIR}
+		sbatch --array=1-100 --mem 8G -p normal extraction_script.sh ${ARRAY_INDEX} ${MODEL_ID} ${ANALYZE_ID}
 		#bash extraction_script.sh ${ARRAY_INDEX} ${FULL_FILE} ${PKL_FILE} ${ROOT_DIR}
 		START_INDEX=$(expr ${LINE_COUNT} + 1)
 		ARRAY_INDEX=$(expr ${ARRAY_INDEX} + 1)
@@ -27,6 +28,6 @@ if [ "${LINE_COUNT}" -ge "${START_INDEX}" ]
 then
 	DIFF=$(expr ${LINE_COUNT} - ${START_INDEX} + 1)
 	echo "New Array For Parameters from ${START_INDEX} to ${LINE_COUNT}"
-	sbatch --array=1-${DIFF} --mem 8G -p normal extraction_script.sh ${ARRAY_INDEX} ${FULL_FILE} ${PKL_FILE} ${ROOT_DIR}
+	sbatch --array=1-${DIFF} --mem 8G -p normal extraction_script.sh ${ARRAY_INDEX} ${MODEL_ID} ${ANALYZE_ID}
 	#bash extraction_script.sh ${ARRAY_INDEX} ${FULL_FILE} ${PKL_FILE} ${ROOT_DIR}
 fi
--- bash/extraction_script.sh
+++ bash/extraction_script.sh
@@ -3,18 +3,11 @@
 #SBATCH -n 1 # one core
 #SBATCH -N 1 # on one node
 #SBATCH -t 5000
-
 ARRAY_ID=$1
-PARAMETER_FILE=$2
-PICKLE_FILE=$3
-ROOT_DIR=$4
-
+MODEL_ID=$2
+ANALYZE_ID=$3
+#
 let FILE_LINE=(100*$ARRAY_ID + $SLURM_ARRAY_TASK_ID)
-
 echo "Line ${FILE_LINE}"
 # Get the relevant line from the parameters
-PARAMETERS=$(sed "${FILE_LINE}q;d" ${PARAMETER_FILE})
-
-
-python /Users/eghbalhosseini/MyCodes/neural_manifolds/extract_data.py ${ROOT_DIR} ${PICKLE_FILE} ${PARAMETERS} 20
-#bash openmind_sample.sh ${PARAMETERS}
+python /Users/eghbalhosseini/MyCodes/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
