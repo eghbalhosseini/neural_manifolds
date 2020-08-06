e2d07feba99e1513472df5c4a2233cddd3e00488
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-08-06 09:37:55 -0400

    bugfix

--- bash/extraction_script.sh
+++ bash/extraction_script.sh
@@ -9,5 +9,13 @@ ANALYZE_ID=$3
 #
 let FILE_LINE=(50*$ARRAY_ID + $SLURM_ARRAY_TASK_ID)
 echo "Line ${FILE_LINE}"
+
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+RESULTCACHING_HOME=/om/user/`whoami`/.result_caching
+export RESULTCACHING_HOME
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
+
 # Get the relevant line from the parameters
-python ~/MyCodes/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
+singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_manifolds_tiny_fz.simg python ~/MyCodes/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
