60be18bb79d997acde37cc71ce5d2678a90ab3ce
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-08-06 09:33:33 -0400

    bugfix

--- bash/extraction_script.sh
+++ bash/extraction_script.sh
@@ -10,4 +10,4 @@ ANALYZE_ID=$3
 let FILE_LINE=(50*$ARRAY_ID + $SLURM_ARRAY_TASK_ID)
 echo "Line ${FILE_LINE}"
 # Get the relevant line from the parameters
-python /Users/eghbalhosseini/MyCodes/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
+python ~/MyCodes/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
