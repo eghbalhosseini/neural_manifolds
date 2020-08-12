68b56db4f52c910b944fda2dc22e585db11aba5d
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-08-12 16:53:54 -0400

    bugfix

--- bash/extraction_pipeline.sh
+++ bash/extraction_pipeline.sh
@@ -6,7 +6,7 @@ PTH_FILE="master_NN-tree_nclass=50_nobj=50000_beta=0.01_sigma=1.5_nfeat=3072-tra
 MODEL_ID='[NN]-[tree/nclass=50/nobj=50000/beta=0.01/sigma=1.5/nfeat=3072]-[train_test]-[fixed]'
 ANALYZE_ID='[mftma]-[exm_per_class=50]-[proj=False]-[rand=False]-[kappa=0]-[n_t=300]-[n_rep=1]'
 
-chmod g+w -R "${ROOT_DIR}/${MODEL_DIR}
+chmod g+w -R "${ROOT_DIR}/${MODEL_DIR}"
 
 FULL_FILE="${ROOT_DIR}/${MODEL_DIR}/${PTH_FILE}"
 echo $FULL_FILE
