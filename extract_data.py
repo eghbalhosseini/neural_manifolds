e3fc11d63e3093fed9d88626ad4d33762172427c
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-08-18 20:01:42 -0400

    bugfix

--- extract_data.py
+++ extract_data.py
@@ -104,10 +104,10 @@ if __name__ == '__main__':
         sio.savemat(mat_file_name, {'activation': d_master})
         projection_file_list.append(projection_file+'\n')
     # write to csv file
-    if not os.path.exists(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_extracted.csv')):
-        extracted_files_txt = open(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_extracted.csv'), 'w')
+    if not os.path.exists(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_extracted.txt')):
+        extracted_files_txt = open(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_extracted.txt'), 'w')
         extracted_files_txt.writelines(projection_file_list)
     else:
-        extracted_files_txt = open(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_extracted.csv'), 'a')
+        extracted_files_txt = open(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_extracted.txt'), 'a')
         extracted_files_txt.writelines(projection_file_list)
     print('done!')
