4145e2500a30957c37a12bc53d25a05b95e1ff06
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-08-25 17:32:50 -0400

    update on mftma pool

--- bash/run_mftma_pool.sh
+++ bash/run_mftma_pool.sh
@@ -5,9 +5,10 @@
 #SBATCH -N 1
 #SBATCH --exclude node017,node018
 
-MODEL_DIR=$1
-MODEL_ID=$MODEL_DIR
-ANALYZE_ID='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1'
+ROOT_DIR=/om/group/evlab/Greta_Eghbal_manifolds/extracted/
+analyze='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1'
+
+MODEL_ID=NN-partition_nclass=96_nobj=96000_nhier=1_beta=0.0_sigma=0.83_nfeat=3072-train_test-fixed
 
 module add openmind/singularity
 export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
--- mftma_pool_results.py
+++ mftma_pool_results.py
@@ -7,16 +7,13 @@ import re
 import numpy as np
 import argparse
 parser = argparse.ArgumentParser(description='run mftma and save results')
-parser.add_argument('model_id', type=str,default='[NN]-[partition/nclass=50/nobj=50000/beta=0.01/sigma=1.5/nfeat=3072]-[train_test]-[test_performance]')
-parser.add_argument('analyze_id', type=str,default='[mftma]-[exm_per_class=20]-[proj=False]-[rand=True]-[kappa=0]-[n_t=300]-[n_rep=1]')
+parser.add_argument('model_id', type=str, default='NN-tree_nclass=64_nobj=64000_nhier=6_beta=0.02_sigma=0.83_nfeat=3072-train_test-fixed')
+parser.add_argument('analyze_id', type=str, default='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1')
 args = parser.parse_args()
 
-
 if __name__ == '__main__':
-
     model_identifier = args.model_id
     analyze_identifier = args.analyze_id
-
     params = train_pool[model_identifier]()
     layer_names = params.get_layer_names()
     model_identifier_for_saving = params.identifier.translate(str.maketrans({'[': '', ']': '', '/': '_'}))
@@ -24,45 +21,36 @@ if __name__ == '__main__':
     analyze_params = analyze_pool[analyze_identifier]()
     analyze_identifier_for_saving = analyze_params.identifier.translate(str.maketrans({'[': '', ']': '', '/': '_'}))
     # find layers
-    extracted_files = glob.glob(os.path.join(save_dir,model_identifier_for_saving, model_identifier_for_saving + '*_mftma_analysis*'))
-
-    # find epochs
-    s=[re.findall('-batchidx=\d+', x)[0] for x in extracted_files]
-    batchidx = [int(x.split('=')[1]) for x in s]
-    # find batch_id
-    epoch_factor=10**np.ceil(np.log10(np.max(batchidx)))
-    s = [re.findall('-epoch=\d+', x)[0] for x in extracted_files]
-    epochidx = [int(x.split('=')[1]) for x in s]
-    epoch_batch=[batchidx[idx]+x*epoch_factor for idx, x in enumerate(epochidx)]
-    sort_id = np.argsort(epoch_batch)
-    sorted_files = [extracted_files[int(x)] for x in list(sort_id)]
+    analysis_files_csv = open(os.path.join(save_dir, model_identifier_for_saving, 'master_' + model_identifier_for_saving + '_mftma_analysis.csv'),'r')
+    analysis_files = analysis_files_csv.read().splitlines()
+    s = [re.findall('/\d+', x)[0] for x in analysis_files]
+    file_id = [int(x.split('/')[1]) for x in s]
+    sorted_files = [analysis_files[x] for x in np.argsort(file_id)]
     # do layerwise saving
+    mftma_pooled = dict()
     for idx, layer in enumerate(layer_names):
         s = np.asarray([int(not not re.findall(layer, x)) for x in sorted_files])
+        layer_files=[sorted_files[int(x)] for x in np.argwhere(s)]
         x_idx=np.argwhere(s)
-        layer_files=[sorted_files[int(x)] for x in list(x_idx)]
-        s = [re.findall('-epoch=\d+', x)[0] for x in layer_files]
-        epochidx = [int(x.split('=')[1]) for x in s]
-        s = [re.findall('-batchidx=\d+', x)[0] for x in layer_files]
-        batchidx = [int(x.split('=')[1]) for x in s]
-        epoch_factor = 10 ** np.ceil(np.log10(np.max(batchidx)))
-        epoch_batch = [batchidx[idx] + x * epoch_factor for idx, x in enumerate(epochidx)]
-        assert((np.asarray(epoch_batch)==np.sort(epoch_batch)).all())
         layer_results=[]
         for id_file, file in enumerate(layer_files):
             data_=pickle.load(open(file, 'rb'))
             assert(data_['layer_name']==layer)
-            layer_results.append(dict(mftma=data_['mftma_results'], epoch=epochidx[id_file], batch=batchidx[id_file],
-                 seq=id_file))
+            s =re.findall('-batchidx=\d+', file)
+            batchidx = [int(x.split('=')[1]) for x in s][0]
+            s = re.findall('-epoch=\d+', file)
+            epochidx = [int(x.split('=')[1]) for x in s][0]
 
-        pool_file = os.path.join(save_dir,model_identifier_for_saving, f'{model_identifier_for_saving}_{layer}_mftma_pooled.pkl')
-        d_master = {'layer_results': layer_results,
-                    'analyze_identifier': analyze_identifier,
-                    'model_identifier': model_identifier,
-                    'layer_name': layer,
-                    'files_generated': pool_file}
-        save_dict(d_master, pool_file)
-        print('saved '+pool_file)
+            layer_results.append(dict(mftma=data_['mftma_results'], epoch=epochidx, batch=batchidx,
+                 seq=id_file,train_acc=data_['train_acc'],test_acc=data_['test_acc'] , file=file))
+        mftma_pooled[layer]=layer_results
+    pool_file = os.path.join(save_dir,model_identifier_for_saving, f'{model_identifier_for_saving}_mftma_pooled.pkl')
+    d_master = {'analyze_identifier': analyze_identifier,
+                'model_identifier': model_identifier,
+                'mftma_results': mftma_pooled,
+                'file_generated': pool_file}
+    save_dict(d_master, pool_file)
+    print('saved '+pool_file)
     print('done')
 
 
