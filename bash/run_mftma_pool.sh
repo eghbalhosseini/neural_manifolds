03f32e85050daf97832edc9cb1e132037716b4e4
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-09-23 18:09:43 -0400

    bugfix

--- bash/run_mftma_pool.sh
+++ bash/run_mftma_pool.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 #SBATCH --job-name=mftma_pool
-#SBATCH --array=0-16%8
+#SBATCH --array=0-16
 #SBATCH --time=12:00:00
 #SBATCH -N 1
 #SBATCH --exclude node017,node018
 
 ROOT_DIR=/om/group/evlab/Greta_Eghbal_manifolds/extracted/
+analyze='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1'
 i=0
 for model in NN-partition_nclass=100_nobj=100000_nhier=1_beta=0.0_sigma=0.83_nfeat=3072-train_test-fixed \
              NN-partition_nclass=50_nobj=50000_nhier=1_beta=0.0_sigma=0.83_nfeat=3072-train_test-fixed \
@@ -26,7 +27,7 @@ for model in NN-partition_nclass=100_nobj=100000_nhier=1_beta=0.0_sigma=0.83_nfe
              NN-tree_nclass=96_nobj=96000_nhier=6_beta=0.02_sigma=2.5_nfeat=3072-train_test-fixed ; do
                # combine configs
                     model_list[$i]="$model"
-                    i=$i+
+                    i=$[$i + 1]
 done
 
 module add openmind/singularity
@@ -35,6 +36,6 @@ XDG_CACHE_HOME=/om/user/`whoami`/st
 export XDG_CACHE_HOME
 
 echo "Running model:  ${model_list[$SLURM_ARRAY_TASK_ID]}"
-echo "Running analysis:  $analyze"
+echo "Running analysis:  f$analyze"
 
 singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_manifolds_tiny.simg python /om/user/`whoami`/neural_manifolds/mftma_pool_results.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${analyze}
