cd555a545df3f307eb0ad8c90f9ffc1e0ffcc82d
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-10-29 20:47:00 -0400

    deleting some files

--- bash/extraction_script.sh
+++ bash/extraction_script.sh
@@ -16,4 +16,4 @@ XDG_CACHE_HOME=/om/user/`whoami`/st
 export XDG_CACHE_HOME
 
 # Get the relevant line from the parameters
-singularity exec -B /om:/om /om/user/`whoami`/simg_images/neural_manifolds_tiny.simg python /om/user/`whoami`/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
+singularity exec -B /om:/om /mindhive:/mindhive /om/user/`whoami`/simg_images/neural_manifolds_tiny.simg python /om/user/`whoami`/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
