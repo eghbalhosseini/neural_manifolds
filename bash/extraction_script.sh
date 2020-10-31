162df6e49c5d32c72c4149c2c08f0db37a1008a4
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-10-31 14:09:01 -0400

    clean up the files for running analysis

--- bash/extraction_script.sh
+++ bash/extraction_script.sh
@@ -1,14 +1,44 @@
 #!/bin/bash
 #
-#SBATCH -N 1 # on one node
-#SBATCH -t 2:00:00
-ARRAY_ID=$1
-MODEL_ID=$2
-ANALYZE_ID=$3
+#SBATCH -n 1 # one core
+#SBATCH -t 1:00:00
+#SBATCH --mem=8000
+
+GRAND_FILE=$1
+OVERWRITE='false'
 #
 # this is where we put those lines ,
-let FILE_LINE=(100*$ARRAY_ID + $SLURM_ARRAY_TASK_ID)
-echo "Line ${FILE_LINE}"
+
+if [ -n "$SLURM_ARRAY_TASK_ID" ]; then
+  JID=$SLURM_ARRAY_TASK_ID    # Taking the task ID in a job array as an input parameter.
+else
+  JID=$2        # Taking the task ID as an input parameter.
+fi
+echo "${GRAND_FILE}"
+echo $JID
+
+while IFS=, read -r line_count model_line model analyze analyze_file ; do
+  #echo "line_count ${model}"
+  if [ $JID == $line_count ]
+    then
+      echo "found the right match ${line_count}"
+      run_model_line=$model_line
+      run_model=$model
+      run_analyze=$analyze
+      run_file=$analyze_file
+      do_run=true
+      break
+    else
+      do_run=false
+      #echo "didnt the right match"
+  fi
+
+done <"${GRAND_FILE}"
+
+echo "line ${run_model_line}"
+echo "model ${run_model}"
+echo "analyze ${run_analyze}"
+echo "file to analyze ${run_file}"
 
 module add openmind/singularity
 export SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
@@ -16,4 +46,4 @@ XDG_CACHE_HOME=/om/user/`whoami`/st
 export XDG_CACHE_HOME
 
 # Get the relevant line from the parameters
-singularity exec -B /om:/om /mindhive:/mindhive /om/user/`whoami`/simg_images/neural_manifolds_tiny.simg python /om/user/`whoami`/neural_manifolds/extract_data.py ${FILE_LINE} ${MODEL_ID} ${ANALYZE_ID}
+singularity exec -B /om:/om,/mindhive:/mindhive /om/user/`whoami`/simg_images/neural_manifolds_tiny.simg python /om/user/`whoami`/neural_manifolds/extract_data.py ${run_file} ${run_model_line} ${run_model} ${run_analyze} ${OVERWRITE}
