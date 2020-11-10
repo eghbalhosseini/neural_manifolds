4aeb5b03ff896d8a1aadb7d80073444d79a00a44
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-11-10 16:41:26 -0500

    fixing init

--- utils/__init__.py
+++ utils/__init__.py
@@ -71,7 +71,10 @@ class params:
 data_config=[]
 # Creating tags for training paradigm
 data_structure=[dict(struct='partition',nclass=64,n_hier=1,shape=(1,936)),
-                dict(struct='tree',nclass=64,n_hier=6,shape=(1,936))]
+                dict(struct='tree',nclass=64,n_hier=6,shape=(1,936)),
+                dict(struct='partition', nclass=64, n_hier=1, shape=(1, 3072)),
+                dict(struct='tree', nclass=64, n_hier=6, shape=(1, 3072))
+                ]
 for idx, structure in enumerate(data_structure):
     for beta in ['0.0160']: # ['0.0000','0.0160','0.0330','0.0500']
         for sigma in ['0.8330']: # ['0.0000','0.8330','1.6670','2.5000']
