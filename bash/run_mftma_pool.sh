546d46688fc34a9336796bb38fa878d6130284d4
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-11-11 16:29:51 -0500

    update on the pooling

--- bash/run_mftma_pool.sh
+++ bash/run_mftma_pool.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #SBATCH --job-name=mftma_pool
-#SBATCH --array=0-3
+#SBATCH --array=0-31
 #SBATCH --time=12:00:00
 #SBATCH -N 1
 #SBATCH --exclude node017,node018
@@ -10,15 +10,15 @@ ANALYSIS_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/analyze/
 analyze_mftma='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1'
 i=0
 
-struct_list="partition tree"
-hier_list="1 6"
+struct_list="tree"
+hier_list="6"
 struct_arr=($struct_list)
 hier_arr=($hier_list)
 
-for beta in 0.016 ; do
-  for sigma in 0.833 ; do
+for beta in 0.0 0.016 0.033 0.05 ; do
+  for sigma in 0.0 0.833 1.667 2.5 ; do
     for nclass in 64 96 ; do
-      for idx in 0 1 ; do
+      for idx in 0 ; do
         model="linear_NN-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=3072-train_test-fixed"
         model_list[$i]="$model"
         i=$i+1
