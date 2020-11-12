0c3a6d5a56439422b2e06d2984158832144bfac2
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-11-12 18:01:19 -0500

    adding training for linear and nonlinear

---
+++ bash/run_train_linear_nonlinear.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+#SBATCH --job-name=run_train
+#SBATCH -t 8:00:00
+#SBATCH --array=0-1
+#SBATCH --mem=80000
+#SBATCH --exclude node017,node018
+
+# create a list of config names
+
+ROOT_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/extracted/
+#chmod g+w -R "${ROOT_DIR}"
+
+struct_list="tree"
+hier_list="6"
+struct_arr=($struct_list)
+hier_arr=($hier_list)
+
+i=0
+for beta in 0.0 ; do
+  for sigma in 0.833 ; do
+    for nclass in 64 ; do
+      for idx in 0  ; do
+        for nfeat in 936 ; do
+          for network in linear_NN NN ; do
+
+        model="${network}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=${nfeat}-train_test-fixed"
+        model_list[$i]="$model"
+        i=$i+1
+        done
+      done
+    done
+  done
+done
+# define singularity paths
+module add openmind/singularity
+SINGULARITY_CACHEDIR=/om/user/`whoami`/st/
+export SINGULARITY_CACHEDIR
+#
+XDG_CACHE_HOME=/om/user/`whoami`/st
+export XDG_CACHE_HOME
+# run training
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+echo "Running model:  ${model_list[$SLURM_ARRAY_TASK_ID]}"
+
+#singularity exec --nv -B /om:/om,/mindhive:/mindhive /om/user/`whoami`/simg_images/python36_fz python /om/user/`whoami`/neural_manifolds/train_network_on_synthetic_data.py "${model_list[$SLURM_ARRAY_TASK_ID]}"
+#wait
+# Grant access
+#chmod g+w -R ${ROOT_DIR}${model_list[$SLURM_ARRAY_TASK_ID]}
+
+#NOBATCH --gres=gpu:1 --constraint=high-capacity
