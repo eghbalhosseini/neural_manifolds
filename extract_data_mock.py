3e659392cab37198e9314cce71db317a0e0fb0e3
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-11-12 20:46:50 -0500

    the last bugfix

--- extract_data_mock.py
+++ extract_data_mock.py
@@ -166,7 +166,7 @@ if __name__ == '__main__':
         for hier_idx, value in data_mod.items():
             index_pairs = value['index_pairs'] # the chosen pairs
             index_pairs2 = [np.transpose(np.stack(x)).tolist() for x in index_pairs] # separate out [a1,b1] pairs to two arrays of [a1,...,an] and [b1,...,bn]
-            distance_pairs_in_data[hier_idx] = [[full_dataset[x,:] for x in y] for y in index_pairs2] # get the data representation
+            distance_pairs_in_data[hier_idx] = [[torch.from_numpy(full_dataset[x,:]) for x in y] for y in index_pairs2] # get the data representation
         extract = mftma_extractor()
         distance_pair_activations=[[[extract.extractor(model,x.to(device)) for x in y] for y in v] for k, v in distance_pairs_in_data.items()]
         # STEP 6. create projection dataset
