8ee1cd7fd2d57c874bff87963779c4df76cfec4e
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-12 18:45:33 -0500

    change in memory allocation for carpets

--- bash/run_create_synth_dataset_with_carpets.sh
+++ bash/run_create_synth_dataset_with_carpets.sh
@@ -2,9 +2,9 @@
 
 #  run_create_synth_dataset_with_carpets.sh
 #SBATCH --job-name=synth_data_carpet
-#SBATCH -t 48:00:00
+#SBATCH -t 96:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=120G
+#SBATCH --mem=220G
 #SBATCH --array=0-50
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
