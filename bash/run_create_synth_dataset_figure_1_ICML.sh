0f0135f6159c7bd20e11c8e6015599947cc11bac
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-14 17:15:44 -0500

    bugfix

--- bash/run_create_synth_dataset_figure_1_ICML.sh
+++ bash/run_create_synth_dataset_figure_1_ICML.sh
@@ -3,9 +3,9 @@
 #  run_create_synth_dataset_with_carpets.sh
 #SBATCH --job-name=synth_data_carpet
 #SBATCH -t 96:00:00
-#SBATCH --ntasks=1-50
+#SBATCH --ntasks=1
 #SBATCH --mem=120G
-#SBATCH --array=1
+#SBATCH --array=1-50
 #SBATCH --exclude node017,node018
 #SBATCH --mail-type=ALL
 
