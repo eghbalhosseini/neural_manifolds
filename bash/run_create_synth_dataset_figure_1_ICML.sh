e8b014627d3dea7675cf2ecb87ff41372b71957b
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-14 11:45:39 -0500

    added routine for plotting figure 1 for all datasets

---
+++ bash/run_create_synth_dataset_figure_1_ICML.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#  run_create_synth_dataset_with_carpets.sh
+#SBATCH --job-name=synth_data_carpet
+#SBATCH -t 96:00:00
+#SBATCH --ntasks=1
+#SBATCH --mem=120G
+#SBATCH --array=1-50
+#SBATCH --exclude node017,node018
+#SBATCH --mail-type=ALL
+
+
+
+beta_ids=$(seq 1 50)
+
+echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
+module add mit/matlab/2020a
+matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);\
+addpath(genpath('/om/user/`whoami`/neural_manifolds/matlab/'));\
+betas=logspace(-5,2,50);\
+create_figure_1_ICML_v2_openmind($SLURM_ARRAY_TASK_ID);\
+quit;"
+
+
