d8776a3ee9c0c5f2f3fddb70ad1c626f9138f4fe
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-13 19:04:15 -0500

    bugfix

--- bash/run_create_synth_dataset_with_carpets.sh
+++ bash/run_create_synth_dataset_with_carpets.sh
@@ -41,15 +41,20 @@ sigmas=[5];\
 fprintf('creating structure %s\n',structures{${struct_list[$SLURM_ARRAY_TASK_ID]}});\
 struct=structures{${struct_list[$SLURM_ARRAY_TASK_ID]}};\
 n_class=${n_class_list[$SLURM_ARRAY_TASK_ID]};\
-exm_per_class=1000;n_feat=936;\
+exm_per_class=100;n_feat=936;\
 beta=betas(${beta_list[$SLURM_ARRAY_TASK_ID]});sigma=sigmas(${sigma_list[$SLURM_ARRAY_TASK_ID]});\
 ops=create_synth_data_cholesky_method('structure',struct,'n_class',n_class,'exm_per_class',exm_per_class,'n_feat',n_feat,'save_path',save_path,'beta',beta,'sigma',sigma,'norm',true,'save',false);\
 plot_str=strcat('beta_',num2str(ops.beta),'_sigma_',num2str(ops.sigma),'_','nclass_',num2str(ops.n_class),'_nfeat_',num2str(ops.n_feat),'_exmperclass_',num2str(ops.exm_per_class),'_structure_',ops.structure,'.pdf');\
 plot_tree_decomp(ops.data,ops.data_covar, 'save_path', plot_path, 'plot_str', plot_str,'do_svd',false);\
 ops=compute_class_distance_v2(ops);\
+ops_comp=ops;\
+ops_comp=rmfield(ops_comp,'data_covar');\
 data_loc=strcat(save_path,ops.data_id);\
+data_comp_loc=strrep(data_loc,'.mat','_compressed.mat');\
+save(data_comp_loc,'ops_comp','-v7.3');\
+fprintf('saved compressed data in %s \n',data_comp_loc);\
 save(data_loc,'ops','-v7.3');\
-fprintf('saved data in %s \n',data_loc);\
+fprintf('saved full data in %s \n',data_loc);\
 quit;"
 chmod g+w -R /mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/data
 chmod g+w -R /mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/data/plots
