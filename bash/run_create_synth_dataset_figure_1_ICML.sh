e182321cbad2794881ca45dd7440d31fe08bbdaf
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-15 10:53:58 -0500

    bugfix

--- bash/run_create_synth_dataset_figure_1_ICML.sh
+++ bash/run_create_synth_dataset_figure_1_ICML.sh
@@ -14,7 +14,6 @@ echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 module add mit/matlab/2020a
 matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);\
 addpath(genpath('/om/user/`whoami`/neural_manifolds/matlab/'));\
-betas=logspace(-5,2,50);\
 create_figure_1_ICML_v2_openmind($SLURM_ARRAY_TASK_ID);\
 quit;"
 
