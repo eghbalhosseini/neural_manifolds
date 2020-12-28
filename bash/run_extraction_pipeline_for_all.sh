a3f18618fab42bbbcc47a09b100276fc278957d4
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2020-12-28 16:23:27 -0500

    testing new dir location

--- bash/run_extraction_pipeline_for_all.sh
+++ bash/run_extraction_pipeline_for_all.sh
@@ -18,10 +18,13 @@ for beta in 0.0 ; do
     for nclass in 64 ; do
       for idx in 0 ; do
         for net in NN ; do
+          for train_dir in epochs-10_batch-32_lr-0.001_momentum-0.5_init-gaussian_std-0.0001 \
+                           epochs-10_batch-32_lr-0.002_momentum-0.6_init-gaussian_std-1e-05 \
+                           epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
         model="${net}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=936-train_test-fixed"
         model_list[$i]="$model"
         PTH_FILE="master_${model}.csv"
-        FULL_FILE="${ROOT_DIR}/${model}/${PTH_FILE}"
+        FULL_FILE="${ROOT_DIR}/${model}/${train_dir}/${PTH_FILE}"
         echo $FULL_FILE
         MODEL_LINE=0
         while read line; do
@@ -32,12 +35,13 @@ for beta in 0.0 ; do
 
         i=$i+1
         done
+        done
       done
     done
   done
 done
 
 echo $LINE_COUNT
-nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 150 200 50 extraction_script_distance.sh $GRAND_FILE &
+#nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 150 200 50 extraction_script_distance.sh $GRAND_FILE &
 
 
