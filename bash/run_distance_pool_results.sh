6deb1c41cd95f14053cb112b92f7e597f265aec7
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2021-01-15 13:20:31 -0500

    bugfix

--- bash/run_distance_pool_results.sh
+++ bash/run_distance_pool_results.sh
@@ -7,7 +7,7 @@
 
 ROOT_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/extracted/
 ANALYSIS_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/analyze/
-analyze_mftma='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1'
+analyze_mftma='mftma-exm_per_class=50-proj=False-rand=True-kappa=1e-08-n_t=300-n_rep=5'
 i=0
 
 struct_list="tree"
@@ -19,10 +19,8 @@ for beta in 0.000161 ; do
   for sigma in 5.0 ; do
     for nclass in 64 ; do
       for idx in 0 ; do
-        for net in NN ; do
-          for train_dir in epochs-10_batch-32_lr-0.001_momentum-0.5_init-gaussian_std-0.0001 \
-                           epochs-10_batch-32_lr-0.002_momentum-0.6_init-gaussian_std-1e-05 \
-                           epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
+        for net in NN linear_NN ; do
+          for train_dir in epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
         model="${net}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=936-train_test-fixed"
         model_list[$i]="$model"
         train_dir_list[$i]="$train_dir"
