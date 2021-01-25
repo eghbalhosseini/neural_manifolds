e3635983144ecfd8d1e91fc60c9eabf05b07e733
eghbal hosseini-work macbook <eghbalhosseini@gmail.com>
2021-01-25 16:03:51 -0500

    pooling code for center covariance

---
+++ bash/run_center_covar_pool_for_linear_nonlinear.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+#SBATCH --job-name=covar_pool
+#SBATCH --array=0-1
+#SBATCH --time=12:00:00
+#SBATCH -N 1
+#SBATCH --exclude node017,node018
+
+ROOT_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/extracted/
+ANALYSIS_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/analyze/
+analyze_mftma='mftma-exm_per_class=50-proj=False-rand=True-kappa=1e-08-n_t=300-n_rep=5'
+i=0
+
+struct_list="tree"
+hier_list="6"
+struct_arr=($struct_list)
+hier_arr=($hier_list)
+
+for beta in 0.000161 ; do
+    for sigma in 5.0  ; do
+      for nclass in 64  ; do
+        for net in NN linear_NN ; do
+          for idx in 0 ; do
+            for train_dir in epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
+
+            model="${net}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=936-train_test-fixed"
+            model_list[$i]="$model"
+            train_dir_list[$i]="$train_dir"
+          i=$i+1
+          done
+        done
+      done
+    done
+  done
+done
+
+
+module add openmind/singularity
+export SINGULARITY_CACHEDIR=/om/user/${USER}/st/
+XDG_CACHE_HOME=/om/user/${USER}/st
+export XDG_CACHE_HOME
+
+echo "Running model:  ${model_list[$SLURM_ARRAY_TASK_ID]}"
+echo "Running pooling for :  ${analyze_mftma}"
+
+singularity exec -B /om:/om,/mindhive:/mindhive /om/user/${USER}/simg_images/neural_manifolds_tiny.simg python /om/user/${USER}/neural_manifolds/center_covar_pool_results.py ${model_list[$SLURM_ARRAY_TASK_ID]} ${analyze_mftma} ${train_dir_list[$SLURM_ARRAY_TASK_ID]}
---
+++ center_covar_pool_results.py
@@ -0,0 +1,70 @@
+from utils.model_utils import save_dict
+from utils import save_dir, data_dir, train_pool, analyze_dir
+from utils.analysis_utils import analyze_pool
+import pickle
+import os
+import re
+import numpy as np
+import argparse
+import fnmatch
+parser = argparse.ArgumentParser(description='run mftma and save results')
+parser.add_argument('model_id', type=str, default='NN-tree_nclass=64_nobj=64000_nhier=6_beta=0.02_sigma=0.83_nfeat=3072-train_test-fixed')
+parser.add_argument('analyze_id', type=str, default='mftma-exm_per_class=50-proj=False-rand=False-kappa=0-n_t=300-n_rep=1')
+parser.add_argument('train_id', type=str, default='')
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    model_identifier = args.model_id
+    analyze_identifier = args.analyze_id
+    train_dir_identifier = args.train_id
+    params = train_pool[model_identifier]()
+    layer_names = params.get_layer_names()
+    model_identifier_for_saving = params.identifier.translate(str.maketrans({'[': '', ']': '', '/': '_'}))
+
+    analyze_params = analyze_pool[analyze_identifier]()
+    analyze_identifier_for_saving = analyze_params.identifier.translate(str.maketrans({'[': '', ']': '', '/': '_'}))
+    # find layers
+    # manually walk through the files
+    covar_files = []
+    for file in os.listdir(os.path.join(analyze_dir, analyze_identifier_for_saving,model_identifier_for_saving,train_dir_identifier)):
+        if fnmatch.fnmatch(file, '*_center_covar.pkl'):
+            covar_files.append(os.path.join(analyze_dir, analyze_identifier_for_saving,model_identifier_for_saving,train_dir_identifier, file))
+    s = [re.findall('/\d+', x) for x in covar_files]
+    s = [item for sublist in s for item in sublist]
+    file_id = [int(x.split('/')[1]) for x in s]
+    sorted_files = [covar_files[x] for x in np.argsort(file_id)]
+
+      # do layerwise saving
+    covar_pooled = dict()
+    for idx, layer in enumerate(layer_names):
+        s = np.asarray([int(not not re.findall(layer, x)) for x in sorted_files])
+        layer_files=[sorted_files[int(x)] for x in np.argwhere(s)]
+        x_idx=np.argwhere(s)
+        layer_results=[]
+        for id_file, file in enumerate(layer_files):
+            data_=pickle.load(open(file, 'rb'))
+            assert(data_['layer_name']==layer)
+            s =re.findall('-batchidx=\d+', file)
+            batchidx = [int(x.split('=')[1]) for x in s][0]
+            s = re.findall('-epoch=\d+', file)
+            epochidx = [int(x.split('=')[1]) for x in s][0]
+
+            layer_results.append(dict(center_covar=data_['covar_results'], epoch=epochidx, batch=batchidx,
+                 seq=id_file,train_acc=data_['train_acc'],test_acc=data_['test_acc'] , file=file))
+        covar_pooled[layer]=layer_results
+    pool_file = os.path.join(analyze_dir,analyze_identifier_for_saving,model_identifier_for_saving,train_dir_identifier, f'{model_identifier_for_saving}_center_covar_pooled.pkl')
+    d_master = {'analyze_identifier': analyze_identifier,
+                'model_identifier': model_identifier,
+                'train_identifier': train_dir_identifier,
+                'center_covar_results': covar_pooled,
+                'file_generated': pool_file}
+    save_dict(d_master, pool_file)
+    print('saved '+pool_file)
+    print('done')
+
+
+
+
+
+
+
