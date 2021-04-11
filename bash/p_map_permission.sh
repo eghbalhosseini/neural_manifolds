337ed0b29ca003aedba70c70101d97836e8f7004
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-04-11 12:42:22 -0400

    bugfix

--- bash/p_map_permission.sh
+++ bash/p_map_permission.sh
@@ -12,9 +12,9 @@ source /opt/conda/bin/activate
 
 #conda list
 
-conda env list
-conda deactivate rapids
-conda activate base
+#conda env list
+#conda deactivate rapids
+#conda activate base
 which python
 
 python /om/user/${USER}/neural_manifolds/poincare_map_analysis.py ${model_id} ${train_id} ${analyze_id} ${layer_id} ${hier_id}
