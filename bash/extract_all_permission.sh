42cb4e54f63571c0da8bc3b752f583bba5be380b
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-06 15:49:36 -0400

    random seed is off now, each time make_manifold_data is called a new rand_index is generated

--- bash/extract_all_permission.sh
+++ bash/extract_all_permission.sh
@@ -12,4 +12,4 @@ echo "analyze ${run_analyze}"
 echo "file to analyze ${run_file}"
 
 source ~/.bashrc
-python /om/user/${USER}/neural_manifolds/extract_data_mftma_knn_distance.py ${run_file} ${run_model_line} ${run_model} ${run_analyze} ${OVERWRITE}
+python /om/user/${USER}/neural_manifolds/extract_data.py ${run_file} ${run_model_line} ${run_model} ${run_analyze} ${OVERWRITE}
