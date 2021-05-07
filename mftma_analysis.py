0d6d448e5cf4f41caa033548b3927e62a5190dff
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-07 11:19:22 -0400

    bugfix

--- mftma_analysis.py
+++ mftma_analysis.py
@@ -59,7 +59,7 @@ if __name__=='__main__':
     projection_data_ = extracted_data['projection_results']
     # create outputfile
     mftma_file = os.path.join(results_dir,file_parts[-1])
-    mftma_file = mftma_file.replace("_extracted.pkl", '_mftma_analysis.pkl')
+    mftma_file = mftma_file.replace("_extracted_v2.pkl", '_mftma_analysis_v2.pkl')
     #
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # check whether file exist
@@ -75,7 +75,7 @@ if __name__=='__main__':
         mftma_results = run_mftma(projection_data_, kappa=analyze_params.kappa, n_t=analyze_params.n_t, n_reps=analyze_params.n_rep)
         # save results:
         mftma_file=os.path.join(results_dir,file_parts[-1])
-        mftma_file = mftma_file.replace("_extracted.pkl", '_mftma_analysis.pkl')
+        mftma_file = mftma_file.replace("_extracted_v2.pkl", '_mftma_analysis_v2.pkl')
         #print(mftma_file)
     #
         d_master = {'mftma_results': mftma_results,
