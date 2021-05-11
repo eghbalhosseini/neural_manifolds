da88cb356618f1b4a922ede7b320f2eaea8e5be9
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-11 08:25:03 -0400

    testing mftma analysis

--- bash/run_mftma_pipeline_and_remove_dublicate.sh
+++ bash/run_mftma_pipeline_and_remove_dublicate.sh
@@ -16,10 +16,10 @@ hier_list="6"
 struct_arr=($struct_list)
 hier_arr=($hier_list)
 
-for beta in 0.0923671 ; do
+for beta in 0.000161 ; do
   for sigma in 5.0 ; do
     for nclass in 64 ; do
-      for net in linear_NN  ; do
+      for net in NN  ; do
         for idx in 0 ; do
           for train_dir in epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
           model="${net}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=936-train_test-fixed"
@@ -55,8 +55,8 @@ done
 echo $LINE_COUNT
 run_val=0
 if [ "$LINE_COUNT" -gt "$run_val" ]; then
-   #nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 150 200 50 mftma_script.sh $GRAND_MFTMA_FILE &
-   nohup /cm/shared/admin/bin/submit-many-jobs 20 15 20 5 mftma_script.sh $GRAND_MFTMA_FILE &
+   nohup /cm/shared/admin/bin/submit-many-jobs $LINE_COUNT 500 600 100 mftma_script.sh $GRAND_MFTMA_FILE &
+   #nohup /cm/shared/admin/bin/submit-many-jobs 20 15 20 5 mftma_script.sh $GRAND_MFTMA_FILE &
   else
     echo $LINE_COUNT
 fi
