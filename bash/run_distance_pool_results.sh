0e9d0b22ee514e4887a49250ad207d94920ecd43
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-12 17:54:27 -0400

    distance

--- bash/run_distance_pool_results.sh
+++ bash/run_distance_pool_results.sh
@@ -15,11 +15,11 @@ hier_list="6"
 struct_arr=($struct_list)
 hier_arr=($hier_list)
 
-for beta in 0.000161 0.0923671 ; do
+for beta in 0.000161 ; do
   for sigma in 5.0 ; do
     for nclass in 64 ; do
       for idx in 0 ; do
-        for net in NN linear_NN ; do
+        for net in NN ; do
           for train_dir in epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
             model="${net}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=936-train_test-fixed"
         model_list[$i]="$model"
