76f1a0200392de1c78784a23a75ffa536ba18846
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-27 15:10:26 -0400

    bugfix

--- bash/run_check_manifold_capacity.sh
+++ bash/run_check_manifold_capacity.sh
@@ -14,6 +14,6 @@ module add mit/matlab/2018b
 matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);\
 addpath(genpath('/om/user/${USER}/neural_manifolds/matlab/'));\
 save_path='/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/data/';\
-check_simulation_capacity\
+check_simulation_capacity;\
 quit;"
 chmod g+w -R /mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/data
--- matlab/check_simulation_capacity.m
+++ matlab/check_simulation_capacity.m
@@ -7,6 +7,7 @@ mftma_id='mftma-exm_per_class=50-proj=False-rand=True-kappa=1e-08-n_t=300-n_rep=
 network_id='NN-tree_nclass=64_nobj=64000_nhier=6_beta=0.000161_sigma=5.0_nfeat=936-train_test-fixed';
 train_id='epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06';
 file_id='0789_NN-tree_nclass=64_nobj=64000_nhier=6_beta=0.000161_sigma=5.0_nfeat=936-train_test-fixed-epoch=08-batchidx=720_layer_0_Input_extracted_v3.mat';
+sav_id='0789_NN-tree_nclass=64_nobj=64000_nhier=6_beta=0.000161_sigma=5.0_nfeat=936-train_test-fixed-epoch=08-batchidx=720_layer_0_Input_capacity_v3.mat';
 load(strcat(save_path,filesep,mftma_id,filesep,network_id,filesep,train_id,filesep,file_id));
 X = (activation.projection_results{1}.layer_0_Input);
 options.n_rep =10;
@@ -15,3 +16,4 @@ options.flag_NbyM =1;
 for ii=1:30,XtotT{ii} = double(squeeze(X(ii,:,1:20)));end
 [output] = manifold_simcap_analysis(XtotT, options);
 output
+save(strcat(save_path,filesep,mftma_id,filesep,network_id,filesep,train_id,filesep,sav_id),'output');
