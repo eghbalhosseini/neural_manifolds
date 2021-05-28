f9d9cd658454753908036d93ddfb65b66bace095
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-05-28 10:17:31 -0400

    redoing capacity analysis

--- bash/manifold_capacity_matlab_script.sh
+++ bash/manifold_capacity_matlab_script.sh
@@ -49,13 +49,22 @@ matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);\
 addpath(genpath('/om/user/${USER}/neural_manifolds/matlab/'));\
 save_path='/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/extracted/';\
 load(erase('${run_file}',' '));\
-X = (activation.projection_results{1}.(activation.layer_name));\
-for ii=1:min(30,size(X,1)),XtotT{ii} = double(squeeze(X(ii,:,1:20))); end ;\
+output_cell={};\
+for p=1:size(activation.projection_results,2) \
+X = (activation.projection_results{p}.(activation.layer_name));\
+XtotT={};\
+for ii=1:size(X,1),\
+    X_class=double(squeeze(X(ii,:,1:size(X,3))));\
+    modif=0e-2*repmat(randn(size(X_class,1),1),1,size(X_class,2));\
+    XtotT{ii} = X_class;%+modif;\
+end;\
 options.n_rep =10;\
 options.seed0 = 1;\
 options.flag_NbyM =1;\
 [output] = manifold_simcap_analysis(XtotT, options);\
+output_cell=[output_cell;output];\
+end;\
 save_id=strrep(erase('${run_file}',' '),'extracted_v3.mat','capacity_v3.mat');\
-save(save_id,'output');\
+save(save_id,'output_cell');\
 quit;"
 
