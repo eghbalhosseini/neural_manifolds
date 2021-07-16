ec8ab04657477a083b1d75a867bb9cd2109a4de0
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-07-16 15:26:10 -0400

    bugfix

--- matlab/run_manifold_simulation_capacity.m
+++ matlab/run_manifold_simulation_capacity.m
@@ -12,7 +12,13 @@ for p=1:size(activation.projection_results,2)
     options.n_rep =10;
     options.seed0 = 1;
     options.flag_NbyM =1;
-    [output] = manifold_simcap_analysis(XtotT, options);
+    output=[];
+    try
+        [output] = manifold_simcap_analysis(XtotT, options);
+    catch err
+        disp('unable to get output');
+        output=[];
+    end
     output_cell=[output_cell;output];
 end
 save_id=strrep(erase(run_file," "),'extracted_v3.mat','capacity_v3.mat');
