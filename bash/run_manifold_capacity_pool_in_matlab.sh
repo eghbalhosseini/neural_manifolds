cdba3b19fe9b8dd0dfb7b134c833e032bb0062e6
eghbal hosseini @ openmind <ehoseini@mit.edu>
2021-08-06 17:46:22 -0400

    changes

--- bash/run_manifold_capacity_pool_in_matlab.sh
+++ bash/run_manifold_capacity_pool_in_matlab.sh
@@ -38,15 +38,18 @@ addpath(genpath('/om/user/${USER}/neural_manifolds/matlab/'));\
 loc_folder=strcat('${ROOT_DIR}',filesep,'${analyze_mftma}',filesep,'${model_list[$SLURM_ARRAY_TASK_ID]}',filesep,'${train_dir_list[$SLURM_ARRAY_TASK_ID]}');\
 disp(loc_folder);\
 model_identifier='${model_list[$SLURM_ARRAY_TASK_ID]}';\
-d=dir(strcat(loc_folder,filesep,'*_capacity_v3.mat');\
+d=dir(strcat(loc_folder,filesep,'*_capacity_v3.mat'));\
 names=arrayfun(@(x) d(x).name,1:size(d,1),'uni',false);\
 file_num=regexp(names,['\d+_',model_identifier],'match');\
 disp(model_identifier);\
+layer_num=regexp(names,'layer_\w*_capacity','match');\
+layer_num=cellfun(@(x) cell2mat(erase(x,'_capacity')),layer_num,'uni',false);\
 file_num_unorder=cell2mat(cellfun(@(x) str2num(cell2mat(erase(x,['_',model_identifier]))),file_num,'uni',false));\
 [~,order_idx]=sort(file_num_unorder);\
+disp('test');\
 sorted_files=arrayfun(@(x) strcat(d(x).folder,filesep,d(x).name),order_idx,'uni',false);\
 capacity_data_pool=[];\
-for k=1:length(sorted_files),capacity_data_pool=[capacity_data_pool;[file_num_unorder(k),{load(sorted_files{k})}]];end;\
+for k=1:length(sorted_files),capacity_data_pool=[capacity_data_pool;[file_num_unorder(k),layer_num{k},{load(sorted_files{k})}]];end;\
 save_path=strcat(loc_folder,filesep,model_identifier,'_capacity_pooled.mat');\
 save(save_path,'capacity_data_pool');\
 quit;"
