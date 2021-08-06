6da2cb98506f2f3992647a542b10d02a28fba3f6
Eghbal Hosseini <eghbal.hosseini@gmail.com>
2021-08-06 13:31:08 -0400

    manifold capactiy pool

---
+++ bash/run_manifold_capacity_pool_in_matlab.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+#SBATCH --job-name=Pool
+#SBATCH -t 2:00:00
+#SBATCH -N 1
+#SBATCH --array=0
+#SBATCH --exclude node017,node018
+#SBATCH --mail-type=ALL
+
+ROOT_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/extracted/
+ANALYSIS_DIR=/mindhive/evlab/u/Shared/Greta_Eghbal_manifolds/analyze/
+analyze_mftma='mftma-exm_per_class=50-proj=False-rand=True-kappa=1e-08-n_t=300-n_rep=5'
+i=0
+struct_list="tree"
+hier_list="6"
+struct_arr=($struct_list)
+hier_arr=($hier_list)
+
+for beta in 0.000161 ; do
+    for sigma in 5.0  ; do
+      for nclass in 64  ; do
+        for net in NN ; do
+          for idx in 0 ; do
+            for train_dir in epochs-10_batch-32_lr-0.01_momentum-0.5_init-gaussian_std-1e-06 ; do
+            model="${net}-${struct_arr[$idx]}_nclass=${nclass}_nobj=$(($nclass * 1000))_nhier=${hier_arr[$idx]}_beta=${beta}_sigma=${sigma}_nfeat=936-train_test-fixed"
+            model_list[$i]="$model"
+            train_dir_list[$i]="$train_dir"
+            i=$i+1
+          done
+        done
+      done
+    done
+  done
+done
+# implement the code in matlab
+module add mit/matlab/2018b
+matlab -nodisplay -r "maxNumCompThreads($SLURM_NTASKS);\
+addpath(genpath('/om/user/${USER}/neural_manifolds/matlab/'));\
+loc_folder=strcat('${ROOT_DIR}',filesep,'${analyze_mftma}',filesep,'${model_list[$SLURM_ARRAY_TASK_ID]}',filesep,'${train_dir_list[$SLURM_ARRAY_TASK_ID]}');\
+model_identifier='${model_list[$SLURM_ARRAY_TASK_ID]}';\
+d=dir(strcat(loc_folder,filesep,'*_capacity_v3.mat');\
+names=arrayfun(@(x) d(x).name,1:size(d,1),'uni',false)';\
+file_num=regexp(names,['\d+_',model_identifier],'match');\
+file_num_unorder=cell2mat(cellfun(@(x) str2num(cell2mat(erase(x,['_',model_identifier]))),file_num,'uni',false));\
+[~,order_idx]=sort(file_num_unorder);\
+sorted_files=arrayfun(@(x) strcat(d(x).folder,filesep,d(x).name),order_idx,'uni',false);\
+capacity_data_pool=[];\
+for k=1:length(sorted_files)\
+    capacity_data_pool=[capacity_data_pool;[file_num_unorder(k),{load(sorted_files{k})}]];\
+end;\
+save_path=strcat(loc_folder,filesep,model_identifier,'_capacity_pooled.mat');\
+save(save_path,'capacity_data_pool');\
+quit;\"
